3c93d78997fde8a398711d88748ba27c7e0cc128
Shalalalaa <99515332+Shalalalaa@users.noreply.github.com>
2025-01-25 06:31:59 +0000

    fix deploy

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,9 +3,10 @@ export default function AboutPage() {
       <div>
         <h1 className="text-3xl font-bold mt-10">About Me</h1>
         <p className="mt-4">
-          Hi, I'm [Your Name], a developer passionate about creating amazing websites and applications.
+          Hi, I&apos;m a developer passionate about creating amazing websites and applications.
         </p>
       </div>
     );
   }
+  
   
