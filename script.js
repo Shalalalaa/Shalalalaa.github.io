8367f9ea3e25920901f549e4b58a1a28224a2bb1
Shalalalaa <99515332+Shalalalaa@users.noreply.github.com>
2025-03-03 17:34:35 +0000

    change

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 document.getElementById('menu-toggle').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('show');
 });
+
+document.getElementById('toggleViewBtn').addEventListener('click', function() {
+    const projectsContainer = document.getElementById('projectsContainer');
+    const isCarouselView = projectsContainer.classList.contains('carousel-view');
+
+    if (isCarouselView) {
+        projectsContainer.classList.remove('carousel-view');
+        projectsContainer.classList.add('grid-view');
+        this.textContent = "Switch to Carousel View";
+    } else {
+        projectsContainer.classList.remove('grid-view');
+        projectsContainer.classList.add('carousel-view');
+        this.textContent = "Switch to Grid View";
+    }
+});
